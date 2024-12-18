246aa3211fab6d08657ca33e04cfd213959d424a
Thomas de Jong <thomasalexanderdejong@gmail.com>
2024-12-18 13:38:36 +0100

    header changes styling

--- pages/components/Header/Header.tsx
+++ pages/components/Header/Header.tsx
@@ -3,21 +3,22 @@ import styles from './style.module.scss';
 import Link from 'next/link';
 
 interface HeaderProps {
-    [key: string]: unknown;
+  [key: string]: unknown;
 }
 
 const Header = forwardRef<HTMLDivElement, HeaderProps>(function Header(props, ref: Ref<HTMLDivElement>) {
-    return (
-        <header className={styles.header}>
-            <nav className={styles.nav}>
-                <Link href="/">Home</Link>
-                <Link href="/hero">Hero</Link>
-            </nav>
-            <div className={styles.burger}>
-                <div ref={ref} className={styles.bounds}></div>
-            </div>
-        </header>
-    );
+  return (
+    <header className={styles.header}>
+      <h1>Thomasadj98</h1>
+      <nav className={styles.nav}>
+        <Link href="/">Home</Link>
+        <Link href="/hero">Hero</Link>
+      </nav>
+      <div className={styles.burger}>
+        <div ref={ref} className={styles.bounds}></div>
+      </div>
+    </header>
+  );
 });
 
 export default Header;
