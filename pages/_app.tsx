ca632ac93f15c88e03cf91a0257b089121e737b5
Thomas de Jong <thomasalexanderdejong@gmail.com>
2024-12-19 14:51:07 +0100

    create animated menu

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,12 @@
 import "@/styles/globals.css";
-import type { AppProps } from "next/app";
+import type {AppProps} from "next/app";
 import Header from "./components/shared/Header/Header";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return<>
-    <Header />
-    <Component {...pageProps} />
-  </>
-
+export default function App({Component, pageProps}: AppProps) {
+  return (
+    <>
+      <Header/>
+      <Component {...pageProps} />
+    </>
+  )
 }
--- pages/components/shared/Header/Header.tsx
+++ pages/components/shared/Header/Header.tsx
@@ -1,25 +1,112 @@
-import {forwardRef, Ref} from 'react';
+'use client'
+import {Dispatch, SetStateAction, useState} from 'react';
 import styles from './style.module.scss';
 import Link from 'next/link';
+import MenuBtn from '../MenuBtn/MenuBtn'
+import {AnimatePresence, motion} from 'framer-motion';
+import {Links} from '../../../../public/data/navlinksData';
+import {useRouter} from 'next/router';
 
-interface HeaderProps {
-  [key: string]: unknown;
-}
-
-const Header = forwardRef<HTMLDivElement, HeaderProps>(function Header(props, ref: Ref<HTMLDivElement>) {
+export default function Header() {
   return (
     <header className={styles.header}>
-      <h1>Thomasadj98</h1>
-      <nav className={styles.nav}>
-        <Link href="/">Home</Link>
-        <Link href="/hero">Hero</Link>
-        <Link href="/parallax">Parallax</Link>
-      </nav>
-      <div className={styles.burger}>
-        <div ref={ref} className={styles.bounds}></div>
-      </div>
+      <Link href='/'>
+        <h1>Thomasadj98</h1>
+      </Link>
+      <NavMenu/>
     </header>
   );
-});
+}
 
-export default Header;
+
+
+const menuAnimation = {
+  open: {
+    width: 480,
+    height: 650,
+    top: '-25px',
+    right: '-25px',
+    transition: {duration: 0.75, ease: [0.76, 0, 0.24, 1]}
+  },
+  closed: {
+    width: 100,
+    height: 40,
+    top: '0px',
+    right: '0px',
+    transition: {duration: 0.4, delay: 0.35, ease: [0.76, 0, 0.24, 1]}
+  },
+}
+
+function NavMenu() {
+  const [isActive, setIsActive] = useState(false);
+
+  return (
+    <nav className={styles.navMenuContainer}>
+      <motion.div
+        className={styles.navMenu}
+        variants={menuAnimation}
+        animate={isActive ? 'open' : 'closed'}
+        initial='closed'
+      >
+        <AnimatePresence>
+          {isActive && <NavLinks setIsActive={setIsActive}/>}
+        </AnimatePresence>
+
+      </motion.div>
+      <MenuBtn isActive={isActive} setIsActive={setIsActive}/>
+    </nav>
+  )
+}
+
+
+
+const linksAnimation = {
+  initial: {
+    opacity: 0,
+    rotateX: 90,
+    translateY: 80,
+    translateX: -20,
+  },
+  enter: (index: number) => ({
+    opacity: 1,
+    rotateX: 0,
+    translateY: 0,
+    translateX: 0,
+    transition: {
+      duration: 0.65,
+      opacity: {duration: 0.35},
+      delay: 0.4 + (index * 0.1),
+      ease: [.215, .61, .355, 1]
+    }
+  }),
+  exit: {
+    opacity: 0,
+    durarion: {duration: 0.5, ease: [0.76, 0, 0.24, 1]},
+  }
+}
+
+function NavLinks({setIsActive}: { setIsActive: Dispatch<SetStateAction<boolean>> }) {
+  const router = useRouter();
+
+  return (
+    <div className={styles.navLinksContainer}>
+      <div className={styles.body}>
+        {Links.map((link, index) => {
+          return (
+            <div key={index} className={styles.childLinkContainer}>
+              <motion.div
+                custom={index}
+                variants={linksAnimation}
+                animate='enter'
+                exit='exit'
+                initial='initial'>
+                <Link href={link.href} onClick={() => setIsActive(false)}
+                      className={router.pathname === link.href ? styles.active : ''}>{link.title}</Link>
+              </motion.div>
+            </div>
+          )
+        })}
+      </div>
+    </div>
+  )
+}
